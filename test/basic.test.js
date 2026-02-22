787af0a02ee70b5ec28ab01dced7be29becd3e53
rmysliwczyk <31518644+rmysliwczyk@users.noreply.github.com>
2026-02-22 16:37:31 +0000

    Apply automatic changes

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,46 +1,49 @@
-import {By, Builder, until, Browser} from 'selenium-webdriver'
 import assert from 'assert'
+import { Browser, Builder, By, until } from 'selenium-webdriver'
 
 describe('Basic functionality', function () {
-	let driver;
-	before(async function() {
-		driver = await new Builder().forBrowser(Browser.CHROME).build();
-		await driver.manage().setTimeouts({implicit: 5000});
-	});
+	let driver
+	before(async function () {
+		driver = await new Builder().forBrowser(Browser.CHROME).build()
+		await driver.manage().setTimeouts({ implicit: 5000 })
+	})
 
-	it('Title is as expected', async function() {
-		await driver.get('http://127.0.0.1:8000/login');
-		let title = await driver.getTitle();
-		assert.equal("Inventory management system", title);
-		});
+	it('Title is as expected', async function () {
+		await driver.get('http://127.0.0.1:8000/login')
+		let title = await driver.getTitle()
+		assert.equal('Inventory management system', title)
+	})
 
-	it('User can log in', async function() {
-		let usernameTextBox = await driver.findElement(By.name('username'));
-		let passwordTextBox = await driver.findElement(By.name('password'));
-		let submitButton = await driver.findElement(By.css('button'));
-		
-		await usernameTextBox.sendKeys('user');
-		await passwordTextBox.sendKeys('user');
-		await submitButton.click();
+	it('User can log in', async function () {
+		let usernameTextBox = await driver.findElement(By.name('username'))
+		let passwordTextBox = await driver.findElement(By.name('password'))
+		let submitButton = await driver.findElement(By.css('button'))
 
-		let message = await driver.findElement(By.css('h5'));
-		let messageText = await message.getText();
-		assert.equal('Welcome to Inventory Management System. Please select the desired page from the navbar.', messageText);
-		});
+		await usernameTextBox.sendKeys('user')
+		await passwordTextBox.sendKeys('user')
+		await submitButton.click()
 
-	it('User can navigate to Asset Type page', async function() {
-		let buttons = await driver.findElements(By.css('button'));
-		let assetTypePageButton;
+		let message = await driver.findElement(By.css('h5'))
+		let messageText = await message.getText()
+		assert.equal(
+			'Welcome to Inventory Management System. Please select the desired page from the navbar.',
+			messageText
+		)
+	})
+
+	it('User can navigate to Asset Type page', async function () {
+		let buttons = await driver.findElements(By.css('button'))
+		let assetTypePageButton
 		for (let button of buttons) {
-			if ((await button.getText()) == "ASSET TYPE") {
-				assetTypePageButton = button;
-				await assetTypePageButton.click();
-				let message = await driver.findElement(By.css('h5'));
-				let messageText = await message.getText();
-				assert.equal('Asset types:', messageText);
+			if ((await button.getText()) == 'ASSET TYPE') {
+				assetTypePageButton = button
+				await assetTypePageButton.click()
+				let message = await driver.findElement(By.css('h5'))
+				let messageText = await message.getText()
+				assert.equal('Asset types:', messageText)
 			}
 		}
-	});
+	})
 
-	after(async () => await driver.quit());
-});
+	after(async () => await driver.quit())
+})
